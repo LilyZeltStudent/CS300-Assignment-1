60e5017dc2918b698f0cff438f5beb4cbdc6c255
Lily Zelt <46974283+LilyZeltStudent@users.noreply.github.com>
2019-09-01 18:54:21 -0500

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,21 @@
+//Lily Zelt
+//bzgkd@mail.umkc.edu
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	int y1;
+	int y2;
+	for (int i = 0; i <= 100; i += 10) {
+		y1 = (100 * i) + 10;
+		y2 = (5 * i * i) + 2;
+		cout << "y1 = " << y1 << endl;
+		cout << "y2 = " << y2 << endl;
+	}
+
+
+
+	system("pause");
+}
